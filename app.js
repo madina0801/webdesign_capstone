55452c3273a46319b1a8b4b3b432c4ab2b8691b8
Madina <madinatussupova0820@gmail.com>
2023-02-28 10:31:27 +0600

    form validation

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-function validateForm() {
-  const name = document.getElementById("name__input").value;
-  console.log(name);
-  if (name == "") {
-    document.querySelector(".status").innerHTML = "Name cannot be empty";
-    return false;
-  }
-  const email = document.getElementById("email").value;
-  if (email == "") {
-    document.querySelector(".status").innerHTML = "Email cannot be empty";
-    return false;
-  } else {
-    const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(email)) {
-      document.querySelector(".status").innerHTML = "Email format invalid";
-      return false;
-    }
-  }
-  const subject = document.getElementById("subject").value;
-  if (subject == "") {
-    document.querySelector(".status").innerHTML = "Subject cannot be empty";
-    return false;
-  }
-  const message = document.getElementById("message").value;
-  if (message == "") {
-    document.querySelector(".status").innerHTML = "Message cannot be empty";
-    return false;
-  }
-  document.querySelector(".status").innerHTML = "Sending...";
-}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
